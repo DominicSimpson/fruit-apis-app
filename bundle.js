02f639e85b65f80315dd01cf0fcb127e0721ec14
Dominic Simpson <db_simpson@yahoo.co.uk>
2025-07-03 15:28:13 +0100

    Added fruits API

--- bundle.js
+++ bundle.js
@@ -1,7 +1,8 @@
 (function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
 const x = require("./script2")
-const fruitList = document.querySelector("#fruitSection ul")
-const fruitForm = document.querySelector('#inputSection form')
+const fruitList = document.querySelector("#fruitSection ul");
+const fruitForm = document.querySelector('#inputSection form');
+const fruitNutrition = document.querySelector("#nutritionSection p");
 console.log(fruitForm);
 
 fruitForm.addEventListener(
@@ -10,15 +11,33 @@ fruitForm.addEventListener(
 
 function extractFruit(e) {
     e.preventDefault();
-    addFruit(e.target.fruitInput.value);
+    fetchFruitData(e.target.fruitInput.value);
     e.target.fruitInput.value = "";
 }
 
+let cal = 0;
+
 function addFruit(fruit) {
-    const li = document.createElement('li');
-    li.textContent = fruit;
+    const li = document.createElement("li");
+    li.textContent = fruit.name;
+    li.addEventListener("click", removeFruit);
     fruitList.appendChild(li);
+
+    cal += fruit.nutritions.calories;
+    fruitNutrition.textContent = cal;
 }
+
+function removeFruit(e) {
+    e.target.remove();
+}
+
+function fetchFruitData(fruit) {
+    fetch(`https://fruit-api-5v0j.onrender.com/fruits/${fruit}`)
+        .then((resp) => resp.json())
+        .then(data => addFruit(data))
+        .catch((e) => console.log(e));
+}
+
 
 },{"./script2":2}],2:[function(require,module,exports){
 const x = 42;
--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const x = require("./script2")
-const fruitList = document.querySelector("#fruitSection ul")
-const fruitForm = document.querySelector('#inputSection form')
+const fruitList = document.querySelector("#fruitSection ul");
+const fruitForm = document.querySelector('#inputSection form');
+const fruitNutrition = document.querySelector("#nutritionSection p");
 console.log(fruitForm);
 
 fruitForm.addEventListener(
@@ -9,12 +10,30 @@ fruitForm.addEventListener(
 
 function extractFruit(e) {
     e.preventDefault();
-    addFruit(e.target.fruitInput.value);
+    fetchFruitData(e.target.fruitInput.value);
     e.target.fruitInput.value = "";
 }
 
+let cal = 0;
+
 function addFruit(fruit) {
-    const li = document.createElement('li');
-    li.textContent = fruit;
+    const li = document.createElement("li");
+    li.textContent = fruit.name;
+    li.addEventListener("click", removeFruit);
     fruitList.appendChild(li);
+
+    cal += fruit.nutritions.calories;
+    fruitNutrition.textContent = cal;
 }
+
+function removeFruit(e) {
+    e.target.remove();
+}
+
+function fetchFruitData(fruit) {
+    fetch(`https://fruit-api-5v0j.onrender.com/fruits/${fruit}`)
+        .then((resp) => resp.json())
+        .then(data => addFruit(data))
+        .catch((e) => console.log(e));
+}
+
