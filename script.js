2ef8912f3ea7b82c8c7e709b0ba364058a0a7fd8
Dominic Simpson <db_simpson@yahoo.co.uk>
2025-07-06 19:53:43 +0100

    Added comments to script file

--- script.js
+++ script.js
@@ -1,60 +1,59 @@
-const fruitForm = document.querySelector("#inputSection form");
-const fruitList = document.querySelector("#fruitSection ul");
-const fruitNutrition = document.querySelector("#nutritionSection p");
+const fruitForm = document.querySelector("#inputSection form"); // the form the user submits
+const fruitList = document.querySelector("#fruitSection ul"); // where fruit images will be added
+const fruitNutrition = document.querySelector("#nutritionSection p"); // the p that displays the total calories
 
-let cal = 0;
-const fruitCal = {};
+let cal = 0; // running total of all calories
+const fruitCal = {}; // an object that maps fruit names to their individual calorie values (used to subtract later)
 
-fruitForm.addEventListener("submit", extractFruit);
+fruitForm.addEventListener("submit", extractFruit); // When user submits form, extractFruit function is called
 
 function extractFruit(e) {
-    e.preventDefault();
-    fetchFruitData(e.target.fruitInput.value);
-    e.target.fruitInput.value = "";
+    e.preventDefault(); // prevents page from reloading
+    fetchFruitData(e.target.fruitInput.value); // fetches fruit data and image using inputted fruit name
+    e.target.fruitInput.value = ""; // clears the input
 }
 
 const pixabayApiKey = process.env.VITE_PIXABAY_API_KEY;
 
-async function fetchFruitData(fruit) {
+async function fetchFruitData(fruit) { // Make two async fetch API calls
     try {
-        //Make sure to replace this link with your deployed API URL in this fetch
-        const respData = await fetch(`https://fruit-api-5v0j.onrender.com/fruits/${fruit}`);
+        const respData = await fetch(`https://fruit-api-5v0j.onrender.com/fruits/${fruit}`); // for nutrition data
         const respImg = await fetch(
-            `https://pixabay.com/api/?q=${fruit}+fruit&key=${pixabayApiKey}`
+            `https://pixabay.com/api/?q=${fruit}+fruit&key=${pixabayApiKey}` // for image
         );
 
-        if (respData.ok && respImg.ok) {
-            const data = await respData.json();
+        if (respData.ok && respImg.ok) { // if both succeed:
+            const data = await respData.json(); // parse them as JSON
             const imgData = await respImg.json();
-            addFruit(data, imgData);
+            addFruit(data, imgData); // then pass them to addFruit function
         } else {
             throw "Something has gone wrong with one of the API requests";
         }
     } catch (e) {
-        console.log(e);
+        console.log(e); // if either API fais, log error
     }
 }
 
 function addFruit(fruit, fruitImg) {
-    const img = document.createElement("img");
-    img.classList.add('fruits');
-    img.alt = fruit.name;
-    img.src = fruitImg.hits[0].previewURL;
+    const img = document.createElement("img"); // create img element
+    img.classList.add('fruits'); // adds the class name fruits to img element, allowing CSS styling
+    img.alt = fruit.name; // sets alt to the fruit name
+    img.src = fruitImg.hits[0].previewURL; // sets src to image returned from Pixabay
 
-    img.addEventListener("click", removeFruit, { once: true });
-    fruitList.appendChild(img);
+    img.addEventListener("click", removeFruit, { once: true }); // add click listener to remove fruit one at a time
+    fruitList.appendChild(img); // adds newly created fruit img element to DOM
 
-    fruitCal[fruit.name] = fruit.nutritions.calories;
+    fruitCal[fruit.name] = fruit.nutritions.calories; // stores fruit calories in fruitCal under its name
 
-    cal += fruit.nutritions.calories;
-    fruitNutrition.textContent = "Total Calories: " + cal;
+    cal += fruit.nutritions.calories; // updates total calories
+    fruitNutrition.textContent = "Total Calories: " + cal; // displays them
 }
 
 function removeFruit(e) {
-    const fruitName = e.target.alt;
-    cal -= fruitCal[fruitName];
-    fruitNutrition.textContent = "Total Calories: " + cal;
+    const fruitName = e.target.alt; // uses alt of the clicked image to identify fruit
+    cal -= fruitCal[fruitName]; // substracts respective calories from total calories
+    fruitNutrition.textContent = "Total Calories: " + cal; // displays updated calorie amount
 
-    delete fruitCal[fruitName];
-    e.target.remove();
+    delete fruitCal[fruitName]; // removes calories entry and image; delete is JavaScript operator
+    e.target.remove(); // remove() is built-in JavaScript DOM method that removes element from page
 }
